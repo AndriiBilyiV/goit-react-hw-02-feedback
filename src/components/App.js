514fa8623485e25aede0711fda246d288a99a885
AndriiBilyiV <andrey.dobraf@gmail.com>
2023-10-12 22:31:48 +0300

    step 2

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,21 @@ import { Statistics } from "./Statistics/Statistics";
 
 export class App extends Component {
   state = {
-    good: 6,
-    neutral: 0,
+    good: 1,
+    neutral: 2,
     bad: 0
   };
-  countTotalFeedback() {
-    return (this.state.good*2)
+  countTotalFeedback({good, neutral, bad}) {
+    return (good+neutral+bad)
+  };
+  countPositiveFeedbackPercentage({good, neutral, bad}) {
+    return Math.round(good/(good+neutral+bad)*100)
   }
   render() {
     return (
       <div>
         <FeedbackButtons />
-        <Statistics props={this.state} total={this.countTotalFeedback} />
+        <Statistics props={this.state} countTotal={this.countTotalFeedback} goodPercent={this.countPositiveFeedbackPercentage} />
     </div >
       )
   }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,13 +1,15 @@
-export const Statistics = ({props, total}) => {
-    console.log(props)
+export const Statistics = ({props, countTotal, goodPercent}) => {
+    const total = countTotal(props);
+    const percent = goodPercent(props)
     const { good, neutral, bad } = props;
     return (
         <div>
-        <h2>Statistics</h2>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
+            <h2>Statistics</h2>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
             <p>Bad: {bad}</p>
-            <p>Total:{total}</p>
+            <p>Total: {total}</p>
+            <p>Positive feedback {percent}%</p>
         </div>
     )
 }
